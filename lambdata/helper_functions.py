a2131c0a79442ed36984b731f3a765666b6f3688
Mena-E <odemena.ekelemu@gmail.com>
2021-04-06 12:26:09 -0400

    Added code_review.py

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -1,35 +1,48 @@
 """ lambdata_mena2 - A collection of Data Science helper functions"""
 
 
-def null_count(df):
-    """
-    Checks a dataframe for nulls and returns
-    the number of missing values.
-    """
-    return df.isnull().sum().sum() 
-    
+class CleanFrame:
+    def __init__(self, df):
+        self.df = df
 
-def train_test_split_time(df, frac):
-    """
-    Splits a time series dataframe into a training
-    and a testing set based on frac (a fraction) of
-    the percent of the size of the training data,
-    entered in the TimeSeriesSplit class.
-    :return: tuple of training and testing dataframes
-    """
-    val = int(round(len(df) * frac, 0))
-    df_train = df.iloc[0:val, :]
-    df_test = df.iloc[val + 1:, :]
-    return df_train, df_test
+    def null_count(self):
+        """Checks a dataframe for nulls and returns
+           the number of missing values.
+        """
+        return self.df.isnull().sum().sum()
 
 
-def train_test_split_rand(df, frac):
-    """
-    Randomly splits a dataframe into a training and
-    a testing set based on frac value entered in the
-    RandomSplit class.
-    :return: tuple of training and testing dataframes.
-    """
-    df_split1 = df.sample(frac=frac)
-    df_split2 = df.drop(df_split1.index)
-    return df_split1, df_split2
+class TimeSeriesSplit:
+    def __init__(self, df, frac):
+        self.df = df
+        self.frac = float(frac)
+
+    def train_test_split(self):
+        """
+        Splits a time series dataframe into a training
+        and a testing set based on frac (a fraction) of
+        the percent of the size of the training data,
+        entered in the TimeSeriesSplit class.
+        :return: tuple of training and testing dataframes
+        """
+        val = int(round(len(self.df) * self.frac, 0))
+        df_train = self.df.iloc[0:val, :]
+        df_test = self.df.iloc[val + 1:, :]
+        return df_train, df_test
+
+
+class RandomSplit:
+    def __init__(self, df, frac):
+        self.df = df
+        self.frac = float(frac)
+
+    def train_test_split(self):
+        """
+        Randomly splits a dataframe into a training and
+        a testing set based on frac value entered in the
+        RandomSplit class.
+        :return: tuple of training and testing dataframes.
+        """
+        df_split1 = self.df.sample(frac=self.frac)
+        df_split2 = self.df.drop(df_split1.index)
+        return df_split1, df_split2
